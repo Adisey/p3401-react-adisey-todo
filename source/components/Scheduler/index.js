b08f7ad7b07cd7355f57ef7e9fd20264fd4afbab
Andrey Smirnov <iadisey@gmail.com>
2018-07-01 00:21:12 +0300

    Add elements: Header, Input Searh, Input New Task Blank Tasks

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -3,12 +3,29 @@ import React, { Component } from 'react';
 
 // Instruments
 import Styles from './styles.m.css';
+import Task from '../Task';
 
 export default class Scheduler extends Component {
     render () {
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <main>
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input placeholder = 'Поиск' type = 'searh' value = '' />
+                    </header>
+                    <section>
+                        <form>
+                            <input maxLength = '50' placeholder = 'Описaние моей новой задачи' type = 'text' value = '' />
+                            <button>Добавить задачу</button>
+                        </form>
+                        <div>
+                            <Task/>
+                        </div>
+
+                    </section>
+
+                </main>
             </section>
         );
     }
