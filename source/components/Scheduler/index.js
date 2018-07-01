278771085e91f9380eef959a3488bbee447015a7
Andrey Smirnov <iadisey@gmail.com>
2018-07-01 12:38:57 +0300

    Add Spinner

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -3,13 +3,37 @@ import React, { Component } from 'react';
 
 // Instruments
 import Styles from './styles.m.css';
+import Spinner from '../Spinner';
 import Task from '../Task';
 
 export default class Scheduler extends Component {
+    state = {
+        tasks:      [],
+        isSpinning: false,
+    };
+
+    componentDidMount () {
+        console.log('componentDidMount App');
+
+        // Временно прикручен спинер, потом убрать и включить для асинхроннвх операций обращения к серверу.
+        this.setState({ isSpinning: true });
+        setTimeout(() => {
+            this.setState({ isSpinning: false });
+        }, 6000);
+
+    }
+
+
     render () {
+        const { isSpinning } = this.state;
+
+        console.log('isSpinning - ', isSpinning);
+
+
         return (
             <section className = { Styles.scheduler }>
                 <main>
+                    <Spinner isSpinning = { isSpinning } />
                     <header>
                         <h1>Планировщик задач</h1>
                         <input placeholder = 'Поиск' type = 'searh' value = '' />
